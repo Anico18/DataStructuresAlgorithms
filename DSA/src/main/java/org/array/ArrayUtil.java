206fcfe21b40e180e7e2024d76e370388d0679e5
Alejandro Tejada Mesías <80078826+Anico18@users.noreply.github.com>
2024-05-28 21:20:50 -0500

    Print exercise

--- DSA/src/main/java/org/array/ArrayUtil.java
+++ DSA/src/main/java/org/array/ArrayUtil.java
@@ -1,7 +1,18 @@
 package org.array;
 
 public class ArrayUtil {
-    public static void main(String[] args){
+    // Create 'printArray' method. This will print in console each element of the passed array. To do so, this method
+    // finds first the length of the array, and later, iterates through the whole array printing the value.
+    public void printArray(int[] arr){
+        int n = arr.length;
+        for(int i = 0; i < n; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
 
+    public static void main(String[] args){
+        ArrayUtil arrUtil = new ArrayUtil(); // Declaration of the ArrayUtil instance.
+        arrUtil.printArray(new int[] {5, 1, 2, 9, 10}); // Creation on the array.
     }
 }
